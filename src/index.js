1e66c90e1d588d87efe1338e19e8b64185d08366
Chris Bolas <tanlaan33@gmail.com>
2020-11-26 18:46:28 -0800

    Added search text box.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
 import { apiKey } from  './secret.js';
 
-let request = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&s=cat&rating=0`;
+const defaultRequest = makeRequest()
 const img = document.querySelector('img');
-fetch(request, { mode: 'cors' })
-    .then( (response)=> {
-        return response.json();
-    })
-    .then( (response)=> {
-        img.src = response.data.images.original.url;
-    });
+const root = document.querySelector('body')
+
+const newImageInput = document.createElement('input')
+newImageInput.type = 'text'
+root.appendChild(newImageInput)
+
+const newImageButton = document.createElement('button');
+newImageButton.addEventListener('click', newImageHandler)
+newImageButton.textContent = 'New Image'
+newImageButton.type = 'button'
+root.appendChild(newImageButton);
+
+changeImage(defaultRequest, img)
+
+function newImageHandler(event) {
+    let newRequest
+    if(newImageInput.value) {
+        newRequest = makeRequest(newImageInput.value)
+    } else {
+        newRequest = makeRequest()
+    }
+    changeImage(newRequest, img)
+}
+
+function changeImage(request, image) {
+    fetch(request, { mode: 'cors' })
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error('No image found.')
+            }
+        })
+        .then(query => image.src = query.data.images.original.url)
+        .catch(err => console.log(err))
+}
+
+function makeRequest(search = 'cat', endpoint = 'random') {
+    return `https://api.giphy.com/v1/gifs/${endpoint}?api_key=${apiKey}&s=${search}`
+}
